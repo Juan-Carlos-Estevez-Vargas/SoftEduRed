49689ec42c121b3be15de5cd36180d7b502c76a7
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-18 12:53:45 -0500

    mejorando la interfaz visual del inicio de sesión

--- index.php
+++ index.php
@@ -5,69 +5,127 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="images/login.ico" />
-  <link rel="stylesheet" type="text/css" href="style/style_index.css">
-  <META charset="utf-8"/>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous"
+  >
+  <meta charset="utf-8"/>
   <title>Bienvenidos</title>
 </head>
 <body>
-  <div id="contenedor">
-    <?php
-      require_once "Database/conexion.php";
-      $db = database::conectar();
-      session_start();
-      if (isset($_SESSION['active'])) {
-        switch ($_SESSION["FK_ROL"]) {
-          case 'ADMIN':
-            header("location:indexs/index.php?role=e");
-            exit;
-          case 'ATTENDANT':
-            header("location:indexs/index.php?role=att");
-            exit;
-          case 'STUDENT':
-            header("location:indexs/index.php?role=st");
-            exit;
-          case 'TEACHER':
-            header("location:indexs/index.php?role=t");
-            exit;
-          default:
-            exit;
-        }
-      }
-    ?>
+  <section class="">
+    <!-- Jumbotron -->
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+      <div class="container">
+        <?php
+          require_once "persistence/database/Database.php";
+          $db = database::conectar();
+          session_start();
 
-    <img src="style/img1.png" width="500" alt="Iniciar Sesión" />
-    <div id="text">
-      <h1>Iniciar Sesión</h1>
-      <form method="post" action="Login/validacion_login.php">
-        <div id="contenido">
-        <label id="doc">Tipo de Documento de Identidad</label>
-        <select id="form-control" class="form-control" name="tipo_doc">
-          <?php
-            $resultado = $db->query("SELECT * FROM type_of_document");
-            foreach ($resultado as $row):
-          ?>
-          <option value="<?php echo $row['cod_document']; ?>"><?php echo $row['Des_doc'];?></option>
-          <?php endforeach; ?>
-        </select>
+          if (isset($_SESSION['active'])) {
+            $sesion = $_SESSION['FK_ROL'];
 
-        <br />
-        <label id="NO">N° de Documento</label>
-        <label id="user">
-          <input type="text" name="id" id="user" placeholder="Numero De Documento" required />
-        </label>
+            if ($sesion === 'ADMIN') {
+              header("location:utilities/index.php?role=e");
+              exit;
+            } elseif ($sesion === 'ATTENDANT') {
+              header("location:utilities/index.php?role=att");
+              exit;
+            } elseif ($sesion === 'STUDENT') {
+              header("location:utilities/index.php?role=st");
+              exit;
+            } elseif ($sesion === 'TEACHER') {
+              header("location:utilities/index.php?role=t");
+              exit;
+            }
+          }
+        ?>
+        <div class="row gx-lg-5 align-items-center">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <h1 class="my-5 display-3 fw-bold ls-tight">
+              Liceo Campestre de Prueba <br />
+            </h1>
+            <p style="color: hsl(217, 10%, 50.8%)">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+              quibusdam tempora at cupiditate quis eum maiores libero
+              veritatis? Dicta facilis sint aliquid ipsum atque?
+            </p>
+          </div>
 
-        <br />
-        <label id="pass1">Contraseña</label>
-        <label id="pass">
-          <input type="password" name="pass" id="pass" placeholder="Contraseña" required />
-        </label>
-        
-        <br />
-        <button id="ing" type="submit" class="boton" name="a_registro">Iniciar Sesión</button>
-        <a id="olv" href="Login/formulario_rec_contrasena.php" class="b_rcontra">Olvidé mi Contraseña</a><br>
-        <a id="reg" href="Login/registro.php" class="b_regis">Registrarse</a></label>
-      </form>
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="card">
+              <div class="card-body py-5 px-md-5">
+                <form method="POST" action="Login/validacion_login.php">
+                  <h1 class="text-primary mb-4">Iniciar Sesión</h1>
+                  <div class="row">
+                    <div class="col-md-12 mb-4">
+                      <select id="form-control" class="form-control" name="tipo_doc">
+                        <?php
+                          $result = $db->query("SELECT * FROM type_of_document");
+                          foreach ($result as $row):
+                        ?>
+                        <option value="<?php echo $row['cod_document']; ?>"><?php echo $row['Des_doc'];?></option>
+                        <?php endforeach; ?>
+                      </select>
+                      <label id="doc" class="form-label" for="tipo_doc">Tipo de Documento de Identidad</label>
+                    </div>
+
+                    <div class="col-md-6 mb-4">
+                      <div class="form-outline">
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="id"
+                          id="user"
+                          placeholder="Número De Documento"
+                          required
+                        />
+                        <label id="NO" class="form-label" for="id">N° Documento</label>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6 mb-4">
+                      <div class="form-outline">
+                        <input
+                          class="form-control"
+                          type="password"
+                          name="pass"
+                          id="pass"
+                          placeholder="Contraseña"
+                          required
+                        />
+                        <label class="form-label" for="pass">Contraseña</label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row mb-4">
+                    <div class="col">
+                      <button id="ing" type="submit" class="btn btn-primary btn-block col" name="a_registro">
+                        Iniciar Sesión
+                      </button>
+                    </div>
+                    <div class="col">
+                      <a id="reg" href="Login/registro.php" class="btn btn-success btn-block col">
+                        Registrarse
+                      </a>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <a id="olv" href="Login/formulario_rec_contrasena.php">Olvidé mi contraseña</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+    <!-- Jumbotron -->
+  </section>
 </body>
 </html>
