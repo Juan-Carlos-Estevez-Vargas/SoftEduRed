87ca77ab64e1bea828483a08b884e1c715a9bc79
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-19 16:00:21 -0500

    mejora en los botones de las acciones de la página principal

--- utilities/index.php
+++ utilities/index.php
@@ -1,24 +1,33 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-<link rel="shortcut icon" href="../images/student-attendant.ico" />
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title><?php session_start();?> WELCOME</title>
+  <link rel="shortcut icon" href="../images/student-attendant.ico" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php session_start();?> Bienvenido</title>
 </head>
-<?php
-    if  ($_SESSION['active'] === 1 && isset($_GET['role'])){     
-      $_SESSION['role_index']=$_GET['role'];  ?>
-   <frameset rows="191,*" cols="*" framespacing="0" frameborder="no" border="0">
-     <frame src="botones.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="titulo" />
-     <frame src="marco.php" name="mainFrame" id="mainFrame" title="contenido" />
-   </frameset>
-   <noframes>
-<?php         
-    }else {
-      session_destroy();
-      print "<script>alert(\"No Se Ha Iniciado Sesion\");window.location='../index.php';</script>";
-    }?>
+  <?php
+    if ($_SESSION['active'] === 1 && isset($_GET['role'])) {
+      $_SESSION['role_index']=$_GET['role'];
+  ?>
+  <frameset rows="225,*" cols="*" framespacing="0" frameborder="no">
+    <frame src="buttons.php" name="topFrame" noresize="noresize" id="topFrame" title="titulo"></frame>
+    <frame src="marco.php" name="mainFrame" id="mainFrame" title="contenido"></frame>
+  </frameset>
+
+  <noframes>
+    <?php
+      } else {
+        session_destroy();
+        print "
+          <script>
+            alert(\"No Se Ha Iniciado Sesion\");
+            window.location='../index.php';
+          </script>
+        ";
+      }
+    ?>
+  </noframes>
 <body>
 </body>
-</noframes>
 </html>
--- utilities/roles.php
+++ utilities/roles.php
@@ -1,50 +1,61 @@
-<!DOCTYPE HTML>
-<HTML>
-<HEAD>
-    <link rel="stylesheet" type="text/css" href="style/rol.css">
-    <META charset="utf-8"/>
-    <TITLE>BIENVENIDOS</TITLE>
-</HEAD>
-    <BODY>
-        <?php
-            require_once "../Database/conexion.php";
-            $db = database::conectar();
-            session_start();
-        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/rol.css">
+    <title>Bienvenidos</title>
+</head>
+<body>
+    <?php
+        require_once "../persistence/Database/Database.php";
+        $db = database::conectar();
+        session_start();
+    ?>
+    <div class="select">
+        <label>Seleccione:</label>
+        <select name="slct" onchange="window.open(this.value, '_parent', '')">
+            <option class="rol" selected disabled>Rol:</option>
+            <?php
+                $sql = "
+                    SELECT * FROM user_has_role
+                    WHERE pk_fk_id_user ='".$_SESSION["ID_PERSONA"]."'
+                        AND tdoc_role ='".$_SESSION["TIPO_DOC"]."'
+                        AND state = 1
+                ";
+                $result = $db->query($sql);
+            
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    $rol = stripslashes($row["pk_fk_role"]);
+                    $estate = stripslashes($row["state"]);
 
-        <div class="select">
-        <label>Seleccione:</label> <select name="slct" onchange="window.open(this.value, '_parent', '')">
-            <option class="rol" selected disabled>ROL:</option>
-        
-        <?php
-        $sql = "SELECT * FROM user_has_role WHERE pk_fk_id_user ='".$_SESSION["ID_PERSONA"]."' AND tdoc_role ='".$_SESSION["TIPO_DOC"]."' AND state =1";
-        $result=$db->query($sql);
-        
-        while ($row1=$result->fetch(PDO::FETCH_ASSOC)){
-            $rol=stripslashes($row1["pk_fk_role"]);
-            $estado=stripslashes($row1["state"]);
-            if ($estado === null){
-                print "<script>alert(\"ROL INACTIVO\");window.location='cerrar_session.php';</script>";
-            }
-            
-            if ($rol === 'ADMIN'){   
-                echo "<option value='index.php?role=e'>ADMINISTRADOR</option>";
-            }
-            
-            if ($rol === 'TEACHER'){       
-                echo "<option value='index.php?role=t'>DOCENTE</option>";
-            }
-            
-            if ($rol === 'ATTENDANT'){
-                echo "<option value='index.php?role=att'>ACUDIENTE</option>";
-            }
-            
-            if ($rol === 'STUDENT'){
-                echo "<option value='index.php?role=st'>ESTUDIANTE</option>";
-            }
-        } ?>
-        
+                    if ($estate === null){
+                        print "
+                            <script>
+                                alert(\"ROL INACTIVO\");
+                                window.location='cerrar_session.php';
+                            </script>
+                        ";
+                    }
+                
+                    if ($rol === 'ADMIN') {
+                        echo "<option value='index.php?role=e'>Administrador</option>";
+                    }
+                
+                    if ($rol === 'TEACHER') {
+                        echo "<option value='index.php?role=t'>Docente</option>";
+                    }
+                
+                    if ($rol === 'ATTENDANT') {
+                        echo "<option value='index.php?role=att'>Acudiente</option>";
+                    }
+                
+                    if ($rol === 'STUDENT') {
+                        echo "<option value='index.php?role=st'>Estudiante</option>";
+                    }
+                }
+            ?>
         </select>
-       </div>
-    </BODY>
-</HTML>
+    </div>
+</body>
+</html>
