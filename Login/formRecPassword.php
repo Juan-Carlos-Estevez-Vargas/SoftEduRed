bf6c6b568b9d7f8a4d86e031a072a1224b9ea097
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-18 20:20:55 -0500

    mejora visual formulario para recuperar contraseña

--- Login/formRecPassword.php
+++ Login/formRecPassword.php
@@ -1,40 +1,110 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="../style/style_recu.css">
     <meta charset="utf-8">
-    <title>Remember Password</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/favicon/favicon.ico">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+    >
+    <title>Recuperar Contraseña</title>
 </head>
 <body>
-    <header>Recuperar Password</header>
-    <div id="contenedor">
-    <?php require_once "../persistence/Database/Database.php"; $db = database::conectar(); ?>
-        <div id="text">
-        <form method="post" action="recuperar_pass.php">
-            <label id="Titulo">Tipo De Documento</label>
-            <select id="form-control" class="form-control" name="tipo_doc">
-            <?php
-                foreach ($db->query('SELECT * FROM type_of_document') as $row) {
-                    echo '<option value="'.$row['cod_document'].'">'.$row["Des_doc"].'</option>';
-                }?>
-            </select>
+    <main style="background-color: hsl(0, 0%, 96%)">
+        <section class="container d-flex flex-column">
+            <?php require_once "../persistence/Database/Database.php"; $db = database::conectar(); ?>
+            <div class="row align-items-center justify-content-center text-center g-0 min-vh-100">
+                <div class="col-lg-8 col-md-10 py-8 py-xl-0">
+                    <div class="card shadow">
+                        <div class="card-body p-6">
+                            <div class="mb-4">
+                                <h1 class="mb-1 fw-bold">Recuperar Contraseña</h1>
+                                <p>Complete el formulario para restablecer su contraseña.</p>
+                            </div>
+                            <form  method="post" action="recoveryPassword.php">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="tipo_doc" id="Titulo" class="form-label">Tipo De Documento</label>
+                                        <select id="form-control" class="form-control" name="tipo_doc">
+                                            <?php
+                                                foreach ($db->query('SELECT * FROM type_of_document') as $row) {
+                                                    echo '
+                                                        <option
+                                                            value="'.$row['cod_document'].'">
+                                                            '.$row["Des_doc"].'
+                                                        </option>
+                                                    ';
+                                                }
+                                            ?>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <div class="form-outline">
+                                            <label class="form-label" for="n_id">Número de Documento</label>
+                                            <input
+                                                id="Spacen"
+                                                class="form-control"
+                                                type="number"
+                                                name="n_id"
+                                                placeholder="N° de Identificación"
+                                                required
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
 
-            <label id="n_id">Documento de Identidad</label>
-            <input id="Spacen" type="number" name="n_id" placeholder="N° de Identiicación" required />
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="p_seg" class="form-label">Pregunta de seguridad</label>
+                                        <select id="question" class="form-control" name="p_seg">
+                                            <?php
+                                                foreach ($db->query('
+                                                    SELECT * FROM security_question
+                                                    WHERE state = 1
+                                                ') as $row) {
+                                                    echo '
+                                                        <option
+                                                            value="'.$row['question'].'">
+                                                            '.$row["question"].'
+                                                        </option>
+                                                    ';
+                                                }
+                                            ?>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="n_id" class="form-label">
+                                            Ingrese Su Respuesta De Seguridad
+                                        </label>
+                                        <input
+                                            id="ans"
+                                            class="form-control"
+                                            type="text"
+                                            name="r_seg"
+                                            placeholder="Respuesta De Seguridad"
+                                            required
+                                        />
+                                    </div>
+                                </div>
 
-            <label>Pregunta De Seguridad</label>
-            <select id="question" class="form-control" name="p_seg">
-            <?php
-                foreach ($db->query('SELECT * FROM security_question where state = 1') as $row) {
-                    echo '<option value="'.$row['question'].'">'.$row["question"].'</option>';
-                } ?>
-            </select>
-
-            <label>Ingrese Su Respuesta De Seguridad</label>
-            <input id="ans" type="text" name="r_seg" placeholder="Respuesta De Seguridad" required />
-            <button id="buton" type="submit" class="boton" name="a_registro">Validar</button>
-            <a id="boton" href="../index.php" class="b_rcontra">Regresar</a>
-        </form>
-    </div>
+                                <div class="mb-3 d-grid">
+                                    <button id="buton" type="submit" class="btn btn-primary" name="a_registro">
+                                        Validar
+                                    </button>
+                                </div>
+                                <span>Regresar al <a id="boton" href="../index.php">Login</a></span>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
