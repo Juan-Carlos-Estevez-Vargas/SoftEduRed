c21341cbfbec44b71320c52cd4a8c1d17d273117
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-20 18:38:26 -0500

    mejora interfaz noattendance

--- utilities/buttons.php
+++ utilities/buttons.php
@@ -1,133 +1,136 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../assets/css/buttons.css">
-	<title></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/buttons.css">
+  <title></title>
 </head>
+
 <body>
-	<div id="title">
-		<div>
-			<?php include "roles.php"; if ($_SESSION['role_index'] === 'e'): ?>
-		</div>
-		<p id="header">Bienvenido Administrador</p>
-	</div>
-	<header>
-		<div class="container" id="one">
-			<a href="home.php" target="mainFrame">
-				<img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
-			</a>
-			<p class="text">Inicio</p>
-	  </div>
-		<div class="container" id="two">
-			<a href="cruds.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/register-icon.png" alt="Registros">
-			</a>
-			<p class="text">Registros</p>
-		</div>
-		<div class="container" id="three">
-			<img class="icon" src="../assets/icons/google-plus-icon.png" alt="Google Plus">
-			<p class="text">Contáctenos</p>
-		</div>
-		<div class="container" id="four">
-			<a href="../login/closeSession.php" target="_parent">
-				<img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
-			</a>
-			<p class="text">Cerrar Sesión</p>
-		</div>
-	</header>
+  <div id="title">
+    <div>
+      <?php include "roles.php"; if ($_SESSION['role_index'] === 'e'): ?>
+    </div>
+    <p id="header">Bienvenido Administrador</p>
+  </div>
+  <header>
+    <div class="container" id="one">
+      <a href="home.php" target="mainFrame">
+        <img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
+      </a>
+      <p class="text">Inicio</p>
+    </div>
+    <div class="container" id="two">
+      <a href="cruds.php" target="mainFrame">
+        <img class="icon" src="../assets/icons/register-icon.png" alt="Registros">
+      </a>
+      <p class="text">Registros</p>
+    </div>
+    <div class="container" id="three">
+      <img class="icon" src="../assets/icons/google-plus-icon.png" alt="Google Plus">
+      <p class="text">Contáctenos</p>
+    </div>
+    <div class="container" id="four">
+      <a href="../login/closeSession.php" target="_parent">
+        <img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
+      </a>
+      <p class="text">Cerrar Sesión</p>
+    </div>
+  </header>
   <?php endif;
   	if ($_SESSION['role_index'] === 'att'): ?>
-	<div>
-		<p id="header">Bienvenido Acudiente</p>
-	</div>
-	<header>
-		<div class="container" id="one">
-			<a href="home.php" target="mainFrame">
-				<img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
-			</a>
-			<p class="text">Inicio</p>
-	 	</div>
-		<div class="container" id="two">
-			<a href="../views/atributes/studentAsistance.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
-			</a>
-			<p class="text">Asistencias</p>
-		</div>
-		<div class="container" id="three">
+  <div>
+    <p id="header">Bienvenido Acudiente</p>
+  </div>
+  <header>
+    <div class="container" id="one">
+      <a href="home.php" target="mainFrame">
+        <img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
+      </a>
+      <p class="text">Inicio</p>
+    </div>
+    <div class="container" id="two">
+      <a href="../views/atributes/studentAsistanceView.php" target="mainFrame">
+        <img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
+      </a>
+      <p class="text">Asistencias</p>
+    </div>
+    <div class="container" id="three">
       <a href="userInformation.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
-			</a>
-			<p class="text">Información Personal</p>
-		</div>
-		<div class="container" id="four">
-			<a href="../login/closeSession.php" target="_parent">
-				<img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
-			</a>
-			<p class="text">Cerrar Sesión</p>
-		</div>
-	</header>
+        <img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
+      </a>
+      <p class="text">Información Personal</p>
+    </div>
+    <div class="container" id="four">
+      <a href="../login/closeSession.php" target="_parent">
+        <img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
+      </a>
+      <p class="text">Cerrar Sesión</p>
+    </div>
+  </header>
   <?php endif; if ($_SESSION['role_index'] === 'st'): ?>
-	<div>
-		<p id="header">Bienvenido Estudiante</p>
-	</div>
-	<header>
-		<div class="container" id="one">
-			<a href="home.php" target="mainFrame">
-				<img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
-			</a>
-			<p class="text">Inicio</p>
-	    </div>
-		<div class="container" id="two">
-			<a href="../views/atributes/studentAsistance.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
-			</a>
-			<p class="text">Mis Asistencias</p>
-		</div>
-		<div class="container" id="three">
+  <div>
+    <p id="header">Bienvenido Estudiante</p>
+  </div>
+  <header>
+    <div class="container" id="one">
+      <a href="home.php" target="mainFrame">
+        <img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
+      </a>
+      <p class="text">Inicio</p>
+    </div>
+    <div class="container" id="two">
+      <a href="../views/atributes/studentAsistanceView.php" target="mainFrame">
+        <img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
+      </a>
+      <p class="text">Mis Asistencias</p>
+    </div>
+    <div class="container" id="three">
       <a href="userInformation.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
-			</a>
-			<p class="text">Información Personal</p>
-		</div>
-		<div class="container" id="four">
-			<a href="../login/closeSession.php" target="_parent">
-				<img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
-			</a>
-			<p class="text">Cerrar Sesión</p>
-		</div>
-	</header>
+        <img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
+      </a>
+      <p class="text">Información Personal</p>
+    </div>
+    <div class="container" id="four">
+      <a href="../login/closeSession.php" target="_parent">
+        <img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
+      </a>
+      <p class="text">Cerrar Sesión</p>
+    </div>
+  </header>
   <?php endif; if ($_SESSION['role_index'] === 't'): ?>
-	<div>
-		<p id="header">Bienvenido Profesor</p>
-	</div>
-	<header>
-		<div class="container" id="one">
-			<a href="home.php" target="mainFrame">
-				<img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
-			</a>
-			<p class="text">Inicio</p>
-	    </div>
-		<div class="container" id="two">
-			<a href="../views/atributes/noAttendance.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
-			</a>
-			<p class="text">Asistencias</p>
-		</div>
-		<div class="container" id="three">
+  <div>
+    <p id="header">Bienvenido Profesor</p>
+  </div>
+  <header>
+    <div class="container" id="one">
+      <a href="home.php" target="mainFrame">
+        <img src="../assets/icons/home-icon.png" class="icon" alt="Inicio">
+      </a>
+      <p class="text">Inicio</p>
+    </div>
+    <div class="container" id="two">
+      <a href="../views/atributes/noAttendanceView.php" target="mainFrame">
+        <img class="icon" src="../assets/icons/assistance-icon.png" alt="Asistencias">
+      </a>
+      <p class="text">Asistencias</p>
+    </div>
+    <div class="container" id="three">
       <a href="userInformation.php" target="mainFrame">
-				<img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
-			</a>
-			<p class="text">Información Personal</p>
-		</div>
-		<div class="container" id="four">
-			<a href="../login/closeSession.php" target="_parent">
-				<img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
-			</a>
-			<p class="text">Cerrar Sesión</p>
-		</div>
-	</header>
+        <img class="icon" src="../assets/icons/personal-info-icon.png" alt="Información Personal">
+      </a>
+      <p class="text">Información Personal</p>
+    </div>
+    <div class="container" id="four">
+      <a href="../login/closeSession.php" target="_parent">
+        <img class="icon" src="../assets/icons/logout-icon.png" alt="Cerrar Sesión">
+      </a>
+      <p class="text">Cerrar Sesión</p>
+    </div>
+  </header>
   <?php endif; ?>
 </body>
+
 </html>
--- views/atributes/noAttendanceView.php
+++ views/atributes/noAttendanceView.php
@@ -1,6 +1,6 @@
 <?php
-require_once "../persistence/database/Database.php";
-require_once "../persistence/atributes/NoAttendance.php";
+require_once "../../persistence/database/Database.php";
+require_once "../../persistence/atributes/NoAttendance.php";
 
 session_start();
 if ($_SESSION['role_index'] === 'e'):
@@ -33,75 +33,131 @@ if (isset($_REQUEST['action'])) {
 <head>
   <meta charset="utf-8">
   <title>No Attendance</title>
-  <link rel="stylesheet" href="../style/style_asist.css">
-  <link rel="stylesheet" href="../indexs/style/cruds_style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-  <div id="new">
-    <form action="" method="">
-      <div id="text">
-        <label>Mr.(Ms) Teacher Select One Course: </label>
-        <select name="s">
-          <?php foreach ($db->query('SELECT * FROM course WHERE state= 1') as $row1) { ?>
-            <option value="<?php echo $row1['cod_course']; ?>"> <?php echo $row1['cod_course']; ?></option>
-          <?php } ?>
-        </select>
+  <section class="h-100 bg-dark">
+    <div class="container py-4 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col">
+          <div class="card card-registration my-4">
+            <div class="row g-0">
+              <div class="col-xl-12">
+                <div class="card-body p-md-5 text-black">
+                  <h3 class="mb-5 text-uppercase text-center">Asistencias</h3>
 
-        <button id="boton">Buscar</button>
-        <input id="buton" type="button" value="reset" onclick="window.location.href='formu_view.php?s '" />
+                  <div class="row">
+                    <div class="col-md-12 mb-4">
+                      <form action="" method="">
+                        <div class="row align-items-center">
+                          <label class="col-auto me-2">Mr.(Ms) Teacher Select One Course: </label>
+                          <div class="col-md-6">
+                            <select name="s" class="form-control">
+                              <?php foreach ($db->query('SELECT * FROM course WHERE state= 1') as $row1) { ?>
+                              <option value="<?php echo $row1['cod_course']; ?>"> <?php echo $row1['cod_course']; ?>
+                              </option>
+                              <?php } ?>
+                            </select>
+                          </div>
+                          <div class="col-auto">
+                            <button class="btn btn-primary me-2" id="boton">Buscar</button>
+                            <input class="btn btn-success" id="buton" type="button" value="reset"
+                              onclick="window.location.href='formu_view.php?s '" />
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-12 mb-4">
+                      <?php
+                        $x = @$_GET['s'];
+
+                        $sql1= "
+                          SELECT
+                            pk_fk_tdoc_user,
+                            pk_fk_user_id,
+                            surname,
+                            second_surname,
+                            first_name,
+                            second_name,
+                            fk_cod_course
+                          FROM student
+                          JOIN user ON pk_fk_user_id = id_user AND pk_fk_tdoc_user = pk_fk_cod_doc
+                          WHERE fk_cod_course LIKE '$x'
+                        ";
+
+                        $query = $db->query($sql1);
+
+                        if($query->rowCount() > 0):
+                      ?>
+                      <form action="" method="POST">
+                        <div class="row align-items-center justify-content-center">
+                          <label class="col-auto me-2">Que Materia desea?: </label>
+                          <div class="col-md-4">
+                            <select name="subject" class="form-control">
+                              <?php foreach ($db->query("
+                                SELECT * FROM subject_has_course
+                                WHERE state_sub_course = 1 AND pk_fk_course_stu ='$x'
+                              ") as $row2) { ?>
+                              <option value="<?php echo $row2['pk_fk_te_sub']; ?>"><?php echo $row2['pk_fk_te_sub']; ?>
+                              </option>
+                              <?php } ?>
+                            </select>
+                          </div>
+                          <div class="col-md-12 text-center mt-5">
+                            <h3 class="mb-5 text-uppercase">Estudiantes</h3>
+                            <div class="table-responsive">
+                              <table class="table table-bordered">
+                                <thead>
+                                  <tr>
+                                    <th>Documento</th>
+                                    <th>ID</th>
+                                    <th>Nombre</th>
+                                    <th>Curso</th>
+                                    <th>Seleccionar</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <?php while ($row = $query->fetch(PDO::FETCH_ASSOC)) { ?>
+                                  <tr>
+                                    <td><input class="form-control" id="space" type="text" name="tdoc"
+                                        value="<?php echo $row['pk_fk_tdoc_user']; ?>" readonly></td>
+                                    <td><input class="form-control" id="space" type="text" name="id"
+                                        value="<?php echo $row['pk_fk_user_id']; ?>" readonly></td>
+                                    <td><input class="form-control" id="space" type="text"
+                                        value="<?php echo $row["first_name"], " ", $row["surname"]; ?>" disabled></td>
+                                    <td><input class="form-control" id="space" type="text" name="course"
+                                        value="<?php echo $row['fk_cod_course']; ?>" readonly></td>
+                                    <td><input class="form-control" id="space" type="checkbox"
+                                        name="<?php echo $row['pk_fk_user_id']; ?>">
+                                    </td>
+                                  </tr>
+                                  <?php } ?>
+                                </tbody>
+                              </table>
+                            </div>
+                            <p>
+                              <input id="reg" class="btn btn-primary col-md-4" type="submit" value="Guardar"
+                                onclick="this.form.action='?action=register';">
+                            </p>
+                          </div>
+                        </div>
+                      </form>
+                      <?php endif;?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </form>
-  </div>
-  <?php
-  $x = @$_GET['s'];
-
-  $sql1 = "
-    SELECT
-      pk_fk_tdoc_user,
-      pk_fk_user_id,
-      surname,
-      second_surname,
-      first_name,
-      second_name,
-      fk_cod_course
-    FROM student
-    JOIN user ON pk_fk_user_id = id_user AND pk_fk_tdoc_user = pk_fk_cod_doc
-    WHERE fk_cod_course LIKE '$x'
-  ";
-
-  $query = $db->query($sql1);
-
-  if ($query->rowCount() > 0): ?>
-    <div id="asist">
-      <form action="" method="POST">
-        <label>Que Materia desea?: </label>
-        <select name="subject" class="form-control">
-          <?php foreach ($db->query("
-          SELECT * FROM subject_has_course
-          WHERE state_sub_course = 1 AND pk_fk_course_stu ='$x'
-        ") as $row2) { ?>
-            <option value="<?php echo $row2['pk_fk_te_sub']; ?>"> <?php echo $row2['pk_fk_te_sub']; ?></option>
-          <?php } ?>
-        </select>
-
-        <h2>Estudiantes:</h2>
-        <?php while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-          echo "<p>", "Estudiante: ";
-          ?>
-          <input id="space" type="text" name="tdoc" value="<?php echo $row['pk_fk_tdoc_user']; ?>" readonly />
-          <input id="space" type="text" name="id" value="<?php echo $row['pk_fk_user_id']; ?>" readonly />
-          <input id="space" type="text" value="<?php echo $row["first_name"], " ", $row["surname"]; ?>" disabled />
-          <input id="space" type="text" name="course" value="<?php echo $row['fk_cod_course']; ?>" readonly />
-          <input id="space" type="checkbox" name="<?php echo $row['pk_fk_user_id']; ?>">
-        <?php } ?>
-        <p>
-          <input id="reg" class="btn btn-primary" type="submit" value="Save"
-            onclick="this.form.action = '?action=register';" />
-        </p>
-      </form>
     </div>
-  <?php endif; ?>
+  </section>
 </body>
 
 </html>
