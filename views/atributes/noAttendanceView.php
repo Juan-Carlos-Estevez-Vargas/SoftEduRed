ca1f6a68305fcfe4461c4e16e7274f78d0a67f89
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-20 17:39:23 -0500

    pequeña mejora en la interfaz de la asistencia

--- views/atributes/noAttendanceView.php
+++ views/atributes/noAttendanceView.php
@@ -1,40 +1,42 @@
 <?php
-  require_once "../persistence/atributes/NoAttendance.php";
-  require_once "../Database/conexion.php";
+require_once "../persistence/database/Database.php";
+require_once "../persistence/atributes/NoAttendance.php";
 
-  session_start();
-  if ($_SESSION['role_index'] === 'e'):
-    include "../indexs/cruds.php";
-  endif;
+session_start();
+if ($_SESSION['role_index'] === 'e'):
+  include "../indexs/cruds.php";
+endif;
 
-  $db = database::conectar();
+$db = database::conectar();
 
-  if (isset($_REQUEST['action'])) {
-    $action = $_REQUEST['action'];
+if (isset($_REQUEST['action'])) {
+  $action = $_REQUEST['action'];
 
-    if ($action === 'update') {
-      $update = new NoAttendance();
-			$update->updateGender($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
-    } elseif ($action === 'register') {
-      $insert = new NoAttendance();
-			$insert ->registerAbsence($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
-    } elseif ($action === 'delete') {
-      $eliminar = new NoAttendance();
-			$eliminar->deleteGender($_GET['cod_attendance']);
-    } elseif ($action === 'edit') {
-      $id = $_GET['id_gender'];
-	  }
+  if ($action === 'update') {
+    $update = new NoAttendance();
+    $update->updateGender($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
+  } elseif ($action === 'register') {
+    $insert = new NoAttendance();
+    $insert->registerAbsence($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
+  } elseif ($action === 'delete') {
+    $eliminar = new NoAttendance();
+    $eliminar->deleteGender($_GET['cod_attendance']);
+  } elseif ($action === 'edit') {
+    $id = $_GET['id_gender'];
   }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<meta charset="utf-8">
-	<title>No Attendance</title>
-	<link rel="stylesheet" href="../style/style_asist.css">
-  <link rel="stylesheet" href="../indexs/style/cruds_style.css" >
+  <meta charset="utf-8">
+  <title>No Attendance</title>
+  <link rel="stylesheet" href="../style/style_asist.css">
+  <link rel="stylesheet" href="../indexs/style/cruds_style.css">
 </head>
+
 <body>
   <div id="new">
     <form action="" method="">
@@ -42,7 +44,7 @@
         <label>Mr.(Ms) Teacher Select One Course: </label>
         <select name="s">
           <?php foreach ($db->query('SELECT * FROM course WHERE state= 1') as $row1) { ?>
-          <option value="<?php echo $row1['cod_course']; ?>"> <?php echo $row1['cod_course']; ?></option>
+            <option value="<?php echo $row1['cod_course']; ?>"> <?php echo $row1['cod_course']; ?></option>
           <?php } ?>
         </select>
 
@@ -54,7 +56,7 @@
   <?php
   $x = @$_GET['s'];
 
-  $sql1= "
+  $sql1 = "
     SELECT
       pk_fk_tdoc_user,
       pk_fk_user_id,
@@ -70,35 +72,36 @@
 
   $query = $db->query($sql1);
 
-  if($query->rowCount()>0):?>
-  <div id="asist">
-    <form action="" method="POST">
-      <label>Que Materia desea?: </label>
-      <select name="subject" class="form-control">
-      <?php foreach ($db->query("
+  if ($query->rowCount() > 0): ?>
+    <div id="asist">
+      <form action="" method="POST">
+        <label>Que Materia desea?: </label>
+        <select name="subject" class="form-control">
+          <?php foreach ($db->query("
           SELECT * FROM subject_has_course
           WHERE state_sub_course = 1 AND pk_fk_course_stu ='$x'
         ") as $row2) { ?>
-      <option value="<?php echo $row2['pk_fk_te_sub']; ?>"> <?php echo $row2['pk_fk_te_sub']; ?></option>
-      <?php } ?>
-      </select>
+            <option value="<?php echo $row2['pk_fk_te_sub']; ?>"> <?php echo $row2['pk_fk_te_sub']; ?></option>
+          <?php } ?>
+        </select>
 
-      <h2>Estudiantes:</h2>
-      <?php  while ($row=$query->fetch(PDO::FETCH_ASSOC)) {
-        echo "<p>" ,"Estudiante: ";
-      ?>
-      <input id="space" type="text" name="tdoc" value="<?php echo $row['pk_fk_tdoc_user']; ?>" readonly />
-      <input id="space" type="text" name="id" value="<?php echo $row['pk_fk_user_id']; ?>" readonly />
-      <input id="space" type="text" value="<?php echo $row["first_name"]," ", $row["surname"];?>" disabled />
-      <input id="space" type="text" name="course" value="<?php echo $row['fk_cod_course']; ?>" readonly />
-      <input id="space" type="checkbox" name="<?php echo $row['pk_fk_user_id']; ?>" >
-    <?php } ?>
-      <p>
-        <input id="reg" class="btn btn-primary" type="submit" value="Save"
-            onclick = "this.form.action = '?action=register';"/>
-      </p>
-    </form>
-  </div>
-  <?php endif;?>
+        <h2>Estudiantes:</h2>
+        <?php while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+          echo "<p>", "Estudiante: ";
+          ?>
+          <input id="space" type="text" name="tdoc" value="<?php echo $row['pk_fk_tdoc_user']; ?>" readonly />
+          <input id="space" type="text" name="id" value="<?php echo $row['pk_fk_user_id']; ?>" readonly />
+          <input id="space" type="text" value="<?php echo $row["first_name"], " ", $row["surname"]; ?>" disabled />
+          <input id="space" type="text" name="course" value="<?php echo $row['fk_cod_course']; ?>" readonly />
+          <input id="space" type="checkbox" name="<?php echo $row['pk_fk_user_id']; ?>">
+        <?php } ?>
+        <p>
+          <input id="reg" class="btn btn-primary" type="submit" value="Save"
+            onclick="this.form.action = '?action=register';" />
+        </p>
+      </form>
+    </div>
+  <?php endif; ?>
 </body>
+
 </html>
--- views/atributes/studentAsistance.php
+++ views/atributes/studentAsistance.php
@@ -1,141 +1,172 @@
 <?php
-     require_once "../persistence/atributes/NoAsistance.php";
-     require_once "../Database/conexion.php";
+     require_once "../../persistence/atributes/NoAttendance.php";
+     require_once "../../persistence/Database/Database.php";
      $db = database::conectar();
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-     <title><?php session_start();?> Welcome</title>
-     <link rel="stylesheet" href="../style/style_stu_asist.css" />
-     <link rel="stylesheet" href="../indexs/style/cruds_style.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="images/login.ico" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="utf-8" />
+  <title><?php session_start();?> Welcome</title>
 </head>
+
 <body>
-     <?php
-     if ($_SESSION['role_index'] === 'st'): ?>
-     <div id="asist">
-          <h1>Student <?php echo $_SESSION["NAME"]. " " .$_SESSION["SNAME"]." ".$_SESSION["LASTNAME"]." ".$_SESSION["SLASTNAME"];?></h1>
-          <?php
-               $sql = "
-                    SELECT * FROM no_attendance
-                    WHERE fk_student_tdoc = '".$_SESSION["TIPO_DOC"]."'
-                    AND fk_student_user_id='".$_SESSION["ID_PERSONA"]."'
-               ";
-     
-               $query = $db->query($sql);
-               echo "FALTASTE EL(los) DÍA(s) : ";?>
-          <?php while ($row=$query->fetch(PDO::FETCH_ASSOC)) { ?>
-          <input id="space" type="text" name="tdoc" value="<?php echo $row['fk_student_tdoc']; ?>" disabled />
-          <input id="space" type="text" name="tdoc" value="<?php echo $row['fk_student_user_id']; ?>" disabled />
-          <input id="space" type="text" name="tdoc" value="<?php echo $row['date']; ?>" disabled />
-          <input id="space" type="text" name="tdoc" value="<?php echo $row['fk_sub_has_course']; ?>" disabled />
-          <?php } ?>
-	</div>
-     <?php endif; if ($_SESSION['role_index'] === 'att'): ?>
-     <div id="new">
-          <form action="" method="">
-          <div id="text">
-               <label>Mr.(Ms) Attendant Select One Student: </label>
-               <select name="s" class="form-control">
-               <?php
-               foreach ($db->query("
+  <section class="h-100 bg-dark">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col">
+          <div class="col-xl-12">
+            <h3 class="mb-5 text-uppercase text-center">Listado de Asistencias</h3>
+            <?php if ($_SESSION['role_index'] === 'st'): ?>
+            <h1>Student
+              <?php echo $_SESSION["NAME"]. " " .$_SESSION["SNAME"]." ".$_SESSION["LASTNAME"]." ".$_SESSION["SLASTNAME"];?>
+            </h1>
+            <?php
+              $sql = "
+                SELECT * FROM no_attendance
+                WHERE fk_student_tdoc = '".$_SESSION["TIPO_DOC"]."'
+                AND fk_student_user_id='".$_SESSION["ID_PERSONA"]."'
+              ";
+              $query = $db->query($sql);
+              echo "FALTASTE EL(los) DÍA(s):";
+              while ($row=$query->fetch(PDO::FETCH_ASSOC)) {
+                echo "<input id='space' type='text' name='tdoc' value='".$row['fk_student_tdoc']."' disabled />";
+                echo "<input id='space' type='text' name='tdoc' value='".$row['fk_student_user_id']."' disabled />";
+                echo "<input id='space' type='text' name='tdoc' value='".$row['date']."' disabled />";
+                echo "<input id='space' type='text' name='tdoc' value='".$row['fk_sub_has_course']."' disabled />";
+              }
+            ?>
+            <?php endif; if ($_SESSION['role_index'] === 'att'): ?>
+            <div class="row align-items-center">
+              <div class="col-xl-4">
+                <label>Mr.(Ms) Attendant Select One Student:</label>
+              </div>
+              <div class="col-xl-4">
+                <select name="s" class="form-control form-control-lg">
+                  <?php
+                  foreach ($db->query("
                     SELECT * FROM student
                     JOIN user ON pk_fk_tdoc_user = pk_fk_cod_doc
-                         AND pk_fk_user_id = id_user
+                    AND pk_fk_user_id = id_user
                     WHERE fk_attendat_cod_doc ='".$_SESSION["TIPO_DOC"]."'
-                         AND fk_attendant_id='".$_SESSION["ID_PERSONA"]."'
-               ") as $row1) { ?>
-               <option value="<?php echo $row1['pk_fk_user_id']; ?>"> <?php echo $row1['first_name']." ".$row1['surname']; ?></option>
-               <?php } ?>
-               </select>
+                    AND fk_attendant_id='".$_SESSION["ID_PERSONA"]."'
+                  ") as $row1) {
+                    echo "<option value='".$row1['pk_fk_user_id']."'>".$row1['first_name']." ".$row1['surname']."</option>";
+                  }
+                ?>
+                </select>
+              </div>
+              <div class="col-xl-4">
+                <button id="boton">Buscar</button>
+              </div>
+            </div>
+            <?php endif; ?>
+          </div>
 
-               <button id="boton">Buscar</button>
-               </form>
-               <?php
-                    if (isset($_GET['s'])) {
-                         $_SESSION['estu_id'] = $_GET['s'];
-                    }
+          <div class="col-xl-12">
+            <?php
+               if (isset($_GET['s'])) {
+               $_SESSION['estu_id'] = $_GET['s'];
+               }
 
-                    $sql1= "
+               $sql1= "
+               SELECT * FROM student
+               JOIN course
+                    ON cod_course = fk_cod_course
+               JOIN subject_has_course
+                    ON cod_course = pk_fk_course_stu
+               WHERE pk_fk_user_id
+               LIKE '".@$_SESSION['estu_id']."'
+               AND fk_attendat_cod_doc = '".$_SESSION['TIPO_DOC']."'
+               AND fk_attendant_id ='".$_SESSION["ID_PERSONA"]."'
+               ";
+
+               $query = $db->query($sql1);
+               if ($query->rowCount() > 0 || isset($_SESSION['b'])):
+               $_SESSION['b'] = 1;
+               ?>
+            <form action="" method="">
+              <div class="row align-items-center">
+                <div class="col-xl-4">
+                  <label>Select The Subject For The NO Asistence Query:</label>
+                </div>
+                <div class="col-xl-4">
+                  <select name="sub" class="form-control">
+                    <?php
+                         foreach ($db->query("
                          SELECT * FROM student
                          JOIN course
                               ON cod_course = fk_cod_course
                          JOIN subject_has_course
                               ON cod_course = pk_fk_course_stu
                          WHERE pk_fk_user_id
-                         LIKE '".@$_SESSION['estu_id']."'
-                              AND fk_attendat_cod_doc = '".$_SESSION['TIPO_DOC']."'
-                              AND fk_attendant_id ='".$_SESSION["ID_PERSONA"]."'
-                    ";
-
-                    $query = $db->query($sql1);
-                    if ($query->rowCount() > 0 || isset($_SESSION['b'])):
-                         $_SESSION['b'] = 1
-               ?>
-          
-               <form action="" method="">
-                    <label>Select The Subject For The NO Asistence Query:</label>
-                    <select name="sub" class=form-control>
-                    <?php
-                         foreach ($db->query("
-                              SELECT * FROM student
-                              JOIN course
-                                   ON cod_course = fk_cod_course
-                              JOIN subject_has_course
-                                   ON cod_course = pk_fk_course_stu
-                              WHERE pk_fk_user_id
-                              LIKE '".$_SESSION['estu_id']."'
-                                   AND fk_attendat_cod_doc = '".$_SESSION['TIPO_DOC']."'
-                                   AND fk_attendant_id ='".$_SESSION["ID_PERSONA"]."'
+                         LIKE '".$_SESSION['estu_id']."'
+                         AND fk_attendat_cod_doc = '".$_SESSION['TIPO_DOC']."'
+                         AND fk_attendant_id ='".$_SESSION["ID_PERSONA"]."'
                          ") as $row1) {
+                         echo "<option value='".$row1['pk_fk_te_sub']."'>".$row1['pk_fk_te_sub']."</option>";
+                         }
                     ?>
-                    <option value="<?php echo $row1['pk_fk_te_sub']; ?>"> <?php echo $row1['pk_fk_te_sub']?></option>
-                    <?php } ?>
-                    </select>
-                    <button id="boton">Buscar</button>
-               </form>
-
-               <?php
-                    $sub=@$_GET['sub'];
-                    if(isset($sub)) :
-               ?>
-               <h1>Student</h1>
-               <?php
-                    $sql = "
-                         SELECT
-                              fk_student_tdoc,
-                              fk_student_user_id,
-                              DATE_FORMAT(date, '%Y - %b - %d, %H:%i') AS date
-                         FROM no_attendance
-                         JOIN student
-                              ON fk_student_tdoc = pk_fk_tdoc_user
-                              AND fk_student_user_id = pk_fk_user_id
-                         WHERE fk_attendat_cod_doc = '".$_SESSION["TIPO_DOC"]."'
-                              AND fk_attendant_id='".$_SESSION["ID_PERSONA"]."'
-                              AND fk_sub_has_course='$sub'
-                              AND fk_student_user_id='".$_SESSION['estu_id']."'
-                    ";
-
-                    $query2 = $db->query($sql);
-
-                    if ($query2->rowCount() > 0) {
-                         echo "FALTO EL(los) DÍA(s) : ";
-               ?>
-               <?php while ($row=$query2->fetch(PDO::FETCH_ASSOC)) { ?>
-               <input  id="space" type="text" name="tdoc" value="<?php echo $row['fk_student_tdoc']; ?>" disabled />
-               <input id="space"  type="text" name="id" value="<?php echo $row['fk_student_user_id']; ?>" disabled />
-               <input  id="space" type="text" name="date" value="<?php echo $row['date']; ?>" disabled />
-
-               <?php }
-                    } else {
-                         echo "El Estudiante Seleccionado NO Ha Fallado En Esta Asignatura";
-                    }
-                    endif;
-                    endif;
-               ?>
+                  </select>
+                </div>
+                <div class="col-xl-4">
+                  <button id="boton">Buscar</button>
+                </div>
+              </div>
+            </form>
           </div>
           <?php endif; ?>
-     </div>
+        </div>
+
+      </div>
+      <?php
+     $sub=@$_GET['sub'];
+     if (isset($sub)):
+     ?>
+      <?php
+     $sql = "
+          SELECT
+          fk_student_tdoc,
+          fk_student_user_id,
+          DATE_FORMAT(date, '%Y - %b - %d, %H:%i') AS date
+          FROM no_attendance
+          JOIN student
+          ON fk_student_tdoc = pk_fk_tdoc_user
+          AND fk_student_user_id = pk_fk_user_id
+          WHERE fk_attendat_cod_doc = '".$_SESSION["TIPO_DOC"]."'
+          AND fk_attendant_id='".$_SESSION["ID_PERSONA"]."'
+          AND fk_sub_has_course='$sub'
+          AND fk_student_user_id='".$_SESSION['estu_id']."'
+     ";
+
+     $query2 = $db->query($sql);
+
+     if ($query2->rowCount() > 0) {
+          echo "FALTO EL(los) DÍA(s):";
+          while ($row=$query2->fetch(PDO::FETCH_ASSOC)) {
+          echo "<input id='space' type='text' name='tdoc' value='".$row['fk_student_tdoc']."' disabled />";
+          echo "<input id='space' type='text' name='id' value='".$row['fk_student_user_id']."' disabled />";
+          echo "<input id='space' type='text' name='date' value='".$row['date']."' disabled />";
+          }
+     } else {
+          print "<script>alert(El Estudiante Seleccionado NO Ha Fallado En Esta Asignatura);</script>";
+     }
+     endif;
+     ?>
+    </div>
+  </section>
+
+  <div id="new">
+
+
+
 </body>
+
 </html>
