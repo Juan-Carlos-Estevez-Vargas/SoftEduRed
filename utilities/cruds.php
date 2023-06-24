16a21fcc303967e1eb897c75924445746300cabb
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-24 16:27:32 -0500

    mejora CRUD asistencias

--- utilities/cruds.php
+++ utilities/cruds.php
@@ -1,31 +1,74 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="style/cruds_style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
 </head>
 
 <body>
-  <div class="select">
-    <select name="select_crud" onchange="window.location.href=this.value">
 
-      <option selected disabled>Acción:</option>
-      <option value='../views/atributes/documentTypeView.php'>Tipo de Documento</option>
-      <option value='../views/atributes/genderView.php'>Género</option>
-      <option value='../views/atributes/relationshipView.php'>Parentesco</option>
-      <option value='../views/atributes/questionView.php'>Pregunta de Seguridad</option>
-      <option value='../views/relationship/roleView.php'>Rol</option>
-      <option value='../views/relationship/roleHasUserView.php'>Rol por Usuario</option>
-      <option value='../views/user/userStudentView.php'>Estudiante</option>
-      <option value='../views/user/userTeacherView.php'>Profesor</option>
-      <option value='../views/user/userAttendantView.php'>Acudiente</option>
-      <option value='../views/atributes/subjectView.php'>Materia</option>
-      <option value='../views/atributes/courseView.php'>Curso</option>
-      <option value='../views/relationship/subjectHasCourseView.php'>Materia por Curso</option>
-      <option value='../views/atributes/noAttendanceView.php'>Asistencia</option>
-    </select>
-  </div>
+  <body>
+    <section class="h-100 bg-white">
+      <div class="container py-4 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col">
+            <div class="card card-registration my-4">
+              <div class="row g-0">
+                <div class="col-xl-12">
+                  <div class="card-body p-md-5 text-black" style="background-color: hsl(0, 0%, 96%)">
+                    <h3 class="mb-5 text-uppercase text-center text-primary">Registros</h3>
+
+                    <div class="row">
+                      <div class="col-md-12 mb-4">
+                        <form action="" method="">
+                          <div class="row align-items-center justify-content-center">
+                            <label class="col-auto me-2">Seleccione un Recurso: </label>
+                            <div class="col-md-auto">
+                              <select name="select_crud" class="form-control"
+                                onchange="window.location.href=this.value">
+                                <option selected disabled>Acción:</option>
+                                <option value='../views/atributes/documentTypeView.php'>Tipo de Documento</option>
+                                <option value='../views/atributes/genderView.php'>Género</option>
+                                <option value='../views/atributes/relationshipView.php'>Parentesco</option>
+                                <option value='../views/atributes/questionView.php'>Pregunta de Seguridad</option>
+                                <option value='../views/relationship/roleView.php'>Rol</option>
+                                <option value='../views/relationship/roleHasUserView.php'>Rol por Usuario</option>
+                                <option value='../views/user/userStudentView.php'>Estudiante</option>
+                                <option value='../views/user/userTeacherView.php'>Docente</option>
+                                <option value='../views/user/userAttendantView.php'>Acudiente</option>
+                                <option value='../views/atributes/subjectView.php'>Materia</option>
+                                <option value='../views/atributes/courseView.php'>Curso</option>
+                                <option value='../views/relationship/subjectHasCourseView.php'>Materia por Curso
+                                </option>
+                                <option value='../views/atributes/noAttendanceView.php'>Asistencia</option>
+                              </select>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="bg-light text-center text-lg-start">
+      <div class="text-center p-3" style="background-color: hsl(0, 0%, 96%)">
+        © 2023 Copyright:
+        <a class="text-blue" href="https://github.com/Juan-Carlos-Estevez-Vargas/SoftEduRed">SoftEduRed.com</a>
+      </div>
+    </footer>
+  </body>
 </body>
 
 </html>
--- views/atributes/noAttendanceView.php
+++ views/atributes/noAttendanceView.php
@@ -1,30 +1,27 @@
 <?php
-require_once "../../persistence/database/Database.php";
-require_once "../../persistence/atributes/NoAttendance.php";
+  require_once "../../persistence/database/Database.php";
+  require_once "../../persistence/atributes/NoAttendance.php";
 
-session_start();
-if ($_SESSION['role_index'] === 'e'):
-  include "../indexs/cruds.php";
-endif;
+  session_start();
 
-$db = database::conectar();
+  $db = database::connect();
 
-if (isset($_REQUEST['action'])) {
-  $action = $_REQUEST['action'];
+  if (isset($_REQUEST['action'])) {
+    $action = $_REQUEST['action'];
 
-  if ($action === 'update') {
-    $update = new NoAttendance();
-    $update->updateGender($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
-  } elseif ($action === 'register') {
-    $insert = new NoAttendance();
-    $insert->registerAbsence($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
-  } elseif ($action === 'delete') {
-    $eliminar = new NoAttendance();
-    $eliminar->deleteGender($_GET['cod_attendance']);
-  } elseif ($action === 'edit') {
-    $id = $_GET['id_gender'];
+    if ($action === 'update') {
+      $update = new NoAttendance();
+      $update->updateGender($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
+    } elseif ($action === 'register') {
+      $insert = new NoAttendance();
+      $insert->registerAbsence($_POST["tdoc"], $_POST["id"], $_POST["subject"], $_POST["course"]);
+    } elseif ($action === 'delete') {
+      $eliminar = new NoAttendance();
+      $eliminar->deleteGender($_GET['cod_attendance']);
+    } elseif ($action === 'edit') {
+      $id = $_GET['id_gender'];
+    }
   }
-}
 ?>
 
 <!DOCTYPE html>
@@ -32,27 +29,29 @@ if (isset($_REQUEST['action'])) {
 
 <head>
   <meta charset="utf-8">
-  <title>No Attendance</title>
+  <title>Asistencias</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
 </head>
 
 <body>
-  <section class="h-100 bg-dark">
+  <section class="h-100 bg-white">
     <div class="container py-4 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col">
           <div class="card card-registration my-4">
             <div class="row g-0">
               <div class="col-xl-12">
-                <div class="card-body p-md-5 text-black">
-                  <h3 class="mb-5 text-uppercase text-center">Asistencias</h3>
+                <div class="card-body p-md-5 text-black" style="background-color: hsl(0, 0%, 96%)">
+                  <h3 class="mb-5 text-uppercase text-center text-primary">Asistencias</h3>
 
                   <div class="row">
                     <div class="col-md-12 mb-4">
                       <form action="" method="">
                         <div class="row align-items-center">
-                          <label class="col-auto me-2">Mr.(Ms) Teacher Select One Course: </label>
+                          <label class="col-auto me-2">Seleccione un Curso: </label>
                           <div class="col-md-6">
                             <select name="s" class="form-control">
                               <?php foreach ($db->query('SELECT * FROM course WHERE state= 1') as $row1) { ?>
@@ -64,7 +63,7 @@ if (isset($_REQUEST['action'])) {
                           <div class="col-auto">
                             <button class="btn btn-primary me-2" id="boton">Buscar</button>
                             <input class="btn btn-success" id="buton" type="button" value="reset"
-                              onclick="window.location.href='formu_view.php?s '" />
+                              onclick="window.location.href='noAttendanceView.php?s '" />
                           </div>
                         </div>
                       </form>
@@ -96,22 +95,24 @@ if (isset($_REQUEST['action'])) {
                       ?>
                       <form action="" method="POST">
                         <div class="row align-items-center justify-content-center">
-                          <label class="col-auto me-2">Que Materia desea?: </label>
+                          <label class="col-auto me-2">¿Que Materia desea ver?: </label>
                           <div class="col-md-4">
                             <select name="subject" class="form-control">
                               <?php foreach ($db->query("
-                                SELECT * FROM subject_has_course
-                                WHERE state_sub_course = 1 AND pk_fk_course_stu ='$x'
-                              ") as $row2) { ?>
+                                  SELECT * FROM subject_has_course
+                                  WHERE state_sub_course = 1 AND pk_fk_course_stu ='$x'
+                                ") as $row2) {
+                              ?>
                               <option value="<?php echo $row2['pk_fk_te_sub']; ?>"><?php echo $row2['pk_fk_te_sub']; ?>
                               </option>
                               <?php } ?>
                             </select>
                           </div>
                           <div class="col-md-12 text-center mt-5">
-                            <h3 class="mb-5 text-uppercase">Estudiantes</h3>
+                            <h3 class="mb-5 text-uppercase text-primary">Inasistencias</h3>
                             <div class="table-responsive">
                               <table class="table table-bordered">
+                                <caption class="text-center">Listado de Resultados</caption>
                                 <thead>
                                   <tr>
                                     <th>Documento</th>
@@ -147,7 +148,9 @@ if (isset($_REQUEST['action'])) {
                           </div>
                         </div>
                       </form>
-                      <?php endif;?>
+                      <?php else: ?>
+                      <h4>No se encontraron registros</h4>
+                      <?php endif; ?>
                     </div>
                   </div>
                 </div>
@@ -155,9 +158,12 @@ if (isset($_REQUEST['action'])) {
             </div>
           </div>
         </div>
-      </div>
-    </div>
   </section>
+
+
+
+
+
 
   <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3" style="background-color: hsl(0, 0%, 96%)">
