68e3b09ef9637276ef19d580773c8c4b5105dbc4
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-25 16:04:10 -0500

    mostrando las acciones según el rol

--- utilities/roles.php
+++ utilities/roles.php
@@ -19,44 +19,47 @@
     <select name="slct" onchange="window.open(this.value, '_parent', '')">
       <option class="rol" selected disabled>Rol:</option>
       <?php
-                $sql = "
-                    SELECT * FROM user_has_role
-                    WHERE pk_fk_id_user ='".$_SESSION["ID_PERSONA"]."'
-                        AND tdoc_role ='".$_SESSION["TIPO_DOC"]."'
-                        AND state = 1
-                ";
-                $result = $db->query($sql);
-            
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    $rol = stripslashes($row["pk_fk_role"]);
-                    $estate = stripslashes($row["state"]);
+            $sql = "
+            SELECT r.description AS role_description,
+                uhr.state AS user_has_role_state
+            FROM role r
+            JOIN user_has_role uhr
+                ON uhr.role_id = r.id_role
+            WHERE uhr.user_id ='".$_SESSION["ID_USER"]."'
+                AND uhr.state = 1
+            ";
+            $result = $db->query($sql);
+        
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $role = stripslashes($row["role_description"]);
+                $estate = stripslashes($row["user_has_role_state"]);
 
-                    if ($estate === null){
-                        print "
-                            <script>
-                                alert(\"ROL INACTIVO\");
-                                window.location='cerrar_session.php';
-                            </script>
-                        ";
-                    }
-                
-                    if ($rol === 'ADMIN') {
-                        echo "<option value='index.php?role=e'>Administrador</option>";
-                    }
-                
-                    if ($rol === 'TEACHER') {
-                        echo "<option value='index.php?role=t'>Docente</option>";
-                    }
-                
-                    if ($rol === 'ATTENDANT') {
-                        echo "<option value='index.php?role=att'>Acudiente</option>";
-                    }
-                
-                    if ($rol === 'STUDENT') {
-                        echo "<option value='index.php?role=st'>Estudiante</option>";
-                    }
+                if ($estate === null){
+                    print "
+                        <script>
+                            alert(\"ROL INACTIVO\");
+                            window.location='cerrar_session.php';
+                        </script>
+                    ";
                 }
-            ?>
+            
+                if ($rol === 'ADMIN') {
+                    echo "<option value='index.php?role=e'>Administrador</option>";
+                }
+            
+                if ($rol === 'TEACHER') {
+                    echo "<option value='index.php?role=t'>Docente</option>";
+                }
+            
+                if ($rol === 'ATTENDANT') {
+                    echo "<option value='index.php?role=att'>Acudiente</option>";
+                }
+            
+                if ($rol === 'STUDENT') {
+                    echo "<option value='index.php?role=st'>Estudiante</option>";
+                }
+            }
+        ?>
     </select>
   </div>
 </body>
